d606a958e4eb40960183c823b7c36359862734d7
felicetto13 <felice.laterza93@icloud.com>
2022-06-10 16:17:13 +0200

    added new variable isActive and condition in the funcion

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -1,29 +1,34 @@
 const slides = [
     {
+        isActive: false,
         idImage: 1,
         image: 'img/01.jpg',
         title: 'Svezia',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     },
     {
+        isActive: false,
         idImage: 2,
         image: 'img/02.jpg',
         title: 'Svizzera',
         text: 'Lorem ipsum.',
     },
     {
+        isActive: false,
         idImage: 3,
         image: 'img/03.jpg',
         title: 'Gran Bretagna',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     },
     {
+        isActive: false,
         idImage: 4,
         image: 'img/04.jpg',
         title: 'Germania',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
     },
     {
+        isActive: false,
         idImage: 5,
         image: 'img/05.jpg',
         title: 'Paradise',
@@ -52,7 +57,9 @@ const app = new Vue(
                 if (index === 0) {
                     return;
                 } else {
-                    this.slideActive = slides[index-1]
+                    slides[index].isActive = false;
+                    slides[index-1].isActive = true;
+                    this.slideActive = slides[index-1];
                 }
             },
             nextActiveImage() {
@@ -60,6 +67,8 @@ const app = new Vue(
                 if (index === slides.length-1) {
                     return;
                 } else {
+                    slides[index].isActive = false;
+                    slides[index+1].isActive = true;
                     this.slideActive = slides[index+1]
                 }
             }
